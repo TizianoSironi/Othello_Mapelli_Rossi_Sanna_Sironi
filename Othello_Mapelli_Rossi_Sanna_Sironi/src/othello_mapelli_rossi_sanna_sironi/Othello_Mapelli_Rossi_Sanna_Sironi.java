63d614b45fc863145acfc8aa1f65f609958542bd
sironi.tiziano <sironi.tiziano@TAVE14.OSDBSESTO.local>
2017-04-21 13:26:30 +0200

    ho creato il progetto

---
+++ Othello_Mapelli_Rossi_Sanna_Sironi/src/othello_mapelli_rossi_sanna_sironi/Othello_Mapelli_Rossi_Sanna_Sironi.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package othello_mapelli_rossi_sanna_sironi;
+
+/**
+ *
+ * @author sironi.tiziano
+ */
+public class Othello_Mapelli_Rossi_Sanna_Sironi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
