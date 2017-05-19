743451315262d186e38344c2c89d8b79251adbd0
Alessandro Achille Rossi <rossi.alessandroachi@TAVE13.OSDBSESTO.local>
2017-05-19 10:43:18 +0200

    Correzione problemi sul server

--- Othello_Mapelli_Rossi_Sanna_Sironi/src/model/OthelloServer.java
+++ Othello_Mapelli_Rossi_Sanna_Sironi/src/model/OthelloServer.java
@@ -40,55 +40,55 @@ public class OthelloServer extends Thread {
             PrintStream client1Output = new PrintStream (client1.getOutputStream());
             PrintStream client2Output = new PrintStream (client2.getOutputStream());
             
-           
-            if(client1Input.readLine().equals("connection;")){
+                       
+            /*if(client1Input.readLine().equals("connection;")){
                 System.out.println("Primo giocatore connesso, in attesa del secondo giocatore.");
             }
             if(client2Input.readLine().equals("connection;")){
                 System.out.println("Secondo giocatore connesso, inizio la partita");
-            }
-            client1Output.print("start: <"+id+">, <N>;");
+            }*/
+            client1Output.println("start: <"+id+">, <N>;");
             System.out.println("client1Output: start: <"+id+">, <N>;");
-            client2Output.print("start: <"+id+">, <B>;");
+            client2Output.println("start: <"+id+">, <B>;");
             System.out.println("client2Output: start: <"+id+">, <B>;");
             do{
                 do{
-                    client1Output.print("round: <N>;");
+                    client1Output.println("round: <N>;");
                     System.out.println("client1Output: round: <N>;");
-                    client2Output.print("round: <N>;");
+                    client2Output.println("round: <N>;");
                     System.out.println("client2Output: round: <N>;");
                     String messaggioC1 = client1Input.readLine();
                     System.out.println("client1Input: "+messaggioC1);
                     int cordX = Integer.parseInt(messaggioC1.substring(10, 10));
                     int cordY = Integer.parseInt(messaggioC1.substring(15, 15));
                     if (true){ //Se la mossa è valida
-                        client1Output.print("update: <N>, <"+cordX+">, <"+cordY+">;");
+                        client1Output.println("update: <N>, <"+cordX+">, <"+cordY+">;");
                         System.out.println("client1Output: update: <N>, <"+cordX+">, <"+cordY+">;");
-                        client2Output.print("update: <N>, <"+cordX+">, <"+cordY+">;");
+                        client2Output.println("update: <N>, <"+cordX+">, <"+cordY+">;");
                         System.out.println("client2Output: update: <N>, <"+cordX+">, <"+cordY+">;");
                     } else {
-                        client1Output.print("move not valid;");
+                        client1Output.println("move not valid;");
                         System.out.println("client1Output: move not valid;");
                     }
                 }while (false); //Controllare se la mossa è valida
                 
                 
                 while(partita.inCorso()){ //Mentre la partita è in corso e la mossa non è valida
-                    client1Output.print("round: <B>;");
+                    client1Output.println("round: <B>;");
                     System.out.println("client1Output: round: <B>;");
-                    client2Output.print("round: <B>;");
+                    client2Output.println("round: <B>;");
                     System.out.println("client2Output: round: <B>;");
                     String messaggioC2 = client2Input.readLine();
                     System.out.println("client2Input:"+messaggioC2);
                     int cordX = Integer.parseInt(messaggioC2.substring(10, 10));
                     int cordY = Integer.parseInt(messaggioC2.substring(15, 15));
                     if (true){ //Se la mossa è valida
-                        client1Output.print("update: <B>, <"+cordX+">, <"+cordY+">;");
+                        client1Output.println("update: <B>, <"+cordX+">, <"+cordY+">;");
                         System.out.println("client1Output: update: <b>, <"+cordX+">, <"+cordY+">;");
-                        client2Output.print("update: <B>, <"+cordX+">, <"+cordY+">;");
+                        client2Output.println("update: <B>, <"+cordX+">, <"+cordY+">;");
                         System.out.println("client2Output: update: <B>, <"+cordX+">, <"+cordY+">;");
                     } else {
-                        client2Output.print("move not valid;");
+                        client2Output.println("move not valid;");
                         System.out.println("client2Output: move not valid;");
                     }
                 } 
@@ -96,13 +96,11 @@ public class OthelloServer extends Thread {
             } while (partita.inCorso());
             int pedineBianche = partita.getPedineBianche();
             int pedineNere = partita.getPedineNere();
-            client1Output.print("end: black, <"+pedineNere+">, white, <"+pedineBianche+">;");
+            client1Output.println("end: black, <"+pedineNere+">, white, <"+pedineBianche+">;");
             System.out.println("client1Output: end: black, <"+pedineNere+">, white, <"+pedineBianche+">;");
-            client2Output.print("end: black, <"+pedineNere+">, white, <"+pedineBianche+">;");
+            client2Output.println("end: black, <"+pedineNere+">, white, <"+pedineBianche+">;");
             System.out.println("client1Output: end: black, <"+pedineNere+">, white, <"+pedineBianche+">;");
-           
-            
-           // System.out.print("c1 dice: " + c1 + " e c2 pure : " + c2);
+        
         } catch (IOException ex) {
             System.out.println(ex);
         }
